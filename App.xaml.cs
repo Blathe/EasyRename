90df0cf81e98c6761f7e63facef67204498644cb
scott <scottpeters2281@gmail.com>
2020-12-31 18:24:11 -0800

    Adding renaming logic for adding text before and after name.

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,5 +16,10 @@ namespace EasyRename
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Show();
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace EasyRename
     public partial class MainWindow : Window
     {
         //The file we will be using in the Example under the file box.
+        //Save the file name + extension separately so we can format text easier.
         public string exampleFile;
         public string exampleFileExtension;
 
@@ -32,34 +33,26 @@ namespace EasyRename
             InitializeComponent();
         }
 
-        //Drag and Drop functionality for multiple files
-        private void WindowDrop(object sender, DragEventArgs e)
+        public void RefreshFiles()
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            if (files != null)
             {
-                //Grab all files dropped onto the window frame
-                files = (string[])e.Data.GetData(DataFormats.FileDrop);
-
                 foreach (string file in files)
                 {
                     string fileName = System.IO.Path.GetFileName(file);
                     //store file names in the File_Display_Box.
-                    File_Display_Box.Items.Add(fileName).ToString();
+                    File_Display_Box.Items.Add(fileName);
                 }
 
-                if (files != null)
-                {
-                    exampleFile = System.IO.Path.GetFileNameWithoutExtension(files[0]);
-                    exampleFileExtension = System.IO.Path.GetExtension(files[0]);
-                    Example_Text.Text = exampleFile + exampleFileExtension;
-                }
+                exampleFile = System.IO.Path.GetFileNameWithoutExtension(files[0]);
+                exampleFileExtension = System.IO.Path.GetExtension(files[0]);
+                Example_Text.Text = exampleFile + exampleFileExtension;
             }
         }
 
-        //Fired any time the text changes in the AddText box.
-        private void AddText_Changed(object sender, TextChangedEventArgs args)
+        public void UpdateText()
         {
-            if (Example_Text.Text != null)
+            if (Example_Text != null)
             {
                 if (AfterName.IsChecked == true)
                 {
@@ -80,6 +73,24 @@ namespace EasyRename
                     Example_Text.Text = exampleString;
                 }
             }
+        }
+
+        //Drag and Drop functionality for multiple files
+        private void WindowDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //Grab all files dropped onto the window frame
+                files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                RefreshFiles();
+            }
+        }
+
+        //Fired any time the text changes in the AddText box.
+        private void AddText_Changed(object sender, TextChangedEventArgs args)
+        {
+            UpdateText();
         }
 
         private void Cancel_Button_Click(object sender, RoutedEventArgs e)
@@ -113,6 +124,41 @@ namespace EasyRename
                     ReplaceWith_Grid.Visibility = Visibility.Hidden;
                     break;
             }
+        }
+
+        private void RenameButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(string file in files)
+            {
+                if (AfterName.IsChecked == true)
+                {
+                    string name = System.IO.Path.GetFileNameWithoutExtension(file);
+                    string directory = System.IO.Path.GetDirectoryName(file) + "\\";
+                    string extension = System.IO.Path.GetExtension(file);
+                    string newName = directory + name + TextToAdd_Box.Text + extension;
+
+                    System.IO.File.Move(file, newName);
+                }
+                if (BeforeName.IsChecked == true)
+                {
+                    string name = System.IO.Path.GetFileNameWithoutExtension(file);
+                    string directory = System.IO.Path.GetDirectoryName(file) + "\\";
+                    string extension = System.IO.Path.GetExtension(file);
+                    string newName = directory + TextToAdd_Box.Text + name + extension;
+
+                    System.IO.File.Move(file, newName);
+                }
+            }
+        }
+
+        private void AfterName_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateText();
+        }
+
+        private void BeforeName_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateText();
         }
     }
 }

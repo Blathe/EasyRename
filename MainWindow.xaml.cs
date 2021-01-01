8a9649e1439bfec6d60eda505d3101d74e0233bd
scott <scottpeters2281@gmail.com>
2020-12-31 21:54:05 -0800

    Added basic replace text logic, example text improvements, and crash fixes.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,18 @@ namespace EasyRename
         //Save the file name + extension separately so we can format text easier.
         public string exampleFile;
         public string exampleFileExtension;
+        public string exampleString;
+
+        //file name strings
+        public string fileName;
+        public string fileExtension;
+        public string fileDirectory;
+
+        public string textToAddAfter;
+        public string textToAddBefore;
+
+        public string textToReplace;
+        public string replaceTextWith;
 
         //files loaded into the program
         public string[] files;
@@ -39,7 +51,7 @@ namespace EasyRename
             {
                 foreach (string file in files)
                 {
-                    string fileName = System.IO.Path.GetFileName(file);
+                    fileName = System.IO.Path.GetFileName(file);
                     //store file names in the File_Display_Box.
                     File_Display_Box.Items.Add(fileName);
                 }
@@ -52,25 +64,39 @@ namespace EasyRename
 
         public void UpdateText()
         {
-            if (Example_Text != null)
+            if (Example_Text != null && exampleFile != null)
             {
                 if (AfterName.IsChecked == true)
                 {
                     //Reset the example text, then add the text input after the file name.
                     Example_Text.Text = "";
-                    string fileName = exampleFile;
-                    string addingToFileName = TextToAdd_Box.Text;
-                    string exampleString = fileName + addingToFileName + exampleFileExtension;
+                    fileName = exampleFile;
+                    textToAddAfter = TextToAdd_Box.Text;
+                    exampleString = fileName + textToAddAfter + exampleFileExtension;
                     Example_Text.Text = exampleString;
                 }
+
                 if (BeforeName.IsChecked == true)
                 {
                     //Reset the example text, then add the text input before the file name.
                     Example_Text.Text = "";
-                    string fileName = exampleFile;
-                    string addingToFileName = TextToAdd_Box.Text;
-                    string exampleString = addingToFileName + fileName + exampleFileExtension;
+                    fileName = exampleFile;
+                    textToAddBefore = TextToAdd_Box.Text;
+                    exampleString = textToAddBefore + fileName + exampleFileExtension;
                     Example_Text.Text = exampleString;
+                }
+
+                if (RenameSelectionBox.SelectedIndex == 1)
+                {
+                    Example_Text.Text = "";
+                    textToReplace = TextToReplace_Box.Text;
+                    replaceTextWith = ReplaceTextWith_Box.Text;
+                    Example_Text.Text = exampleFile + exampleFileExtension;
+                    if (textToReplace != "" && replaceTextWith != "")
+                    {
+                        exampleString = exampleFile.Replace(textToReplace, replaceTextWith);
+                        Example_Text.Text = exampleString + exampleFileExtension;
+                    }
                 }
             }
         }
@@ -96,6 +122,7 @@ namespace EasyRename
         private void Cancel_Button_Click(object sender, RoutedEventArgs e)
         {
             //Clear all loaded files, and clear the example text under the file box.
+            ClearFiles();
             File_Display_Box.Items.Clear();
             Example_Text.Text = "";
         }
@@ -104,6 +131,7 @@ namespace EasyRename
         private void RenameSelectionBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ToggleGrids(RenameSelectionBox.SelectedIndex);
+            UpdateText();
         }
 
         private void ToggleGrids(int selection)
@@ -128,28 +156,89 @@ namespace EasyRename
 
         private void RenameButton_Click(object sender, RoutedEventArgs e)
         {
-            foreach(string file in files)
+            switch (RenameSelectionBox.SelectedIndex)
             {
-                if (AfterName.IsChecked == true)
-                {
-                    string name = System.IO.Path.GetFileNameWithoutExtension(file);
-                    string directory = System.IO.Path.GetDirectoryName(file) + "\\";
-                    string extension = System.IO.Path.GetExtension(file);
-                    string newName = directory + name + TextToAdd_Box.Text + extension;
+                //Add Text
+                case 0:
+                    if (files != null && files.Length != 0)
+                    {
+                        foreach (string file in files)
+                        {
+                            //Add text to end of file name.
+                            if (AfterName.IsChecked == true)
+                            {
+                                fileName = System.IO.Path.GetFileNameWithoutExtension(file);
+                                fileDirectory = System.IO.Path.GetDirectoryName(file) + "\\";
+                                fileExtension = System.IO.Path.GetExtension(file);
+                                string newName = fileDirectory + fileName + TextToAdd_Box.Text + fileExtension;
 
-                    System.IO.File.Move(file, newName);
-                }
-                if (BeforeName.IsChecked == true)
-                {
-                    string name = System.IO.Path.GetFileNameWithoutExtension(file);
-                    string directory = System.IO.Path.GetDirectoryName(file) + "\\";
-                    string extension = System.IO.Path.GetExtension(file);
-                    string newName = directory + TextToAdd_Box.Text + name + extension;
+                                System.IO.File.Move(file, newName);
+                            }
+                            //Add text to beginning of file name.
+                            if (BeforeName.IsChecked == true)
+                            {
+                                fileName = System.IO.Path.GetFileNameWithoutExtension(file);
+                                fileDirectory = System.IO.Path.GetDirectoryName(file) + "\\";
+                                fileExtension = System.IO.Path.GetExtension(file);
+                                string newName = fileDirectory + TextToAdd_Box.Text + fileName + fileExtension;
 
-                    System.IO.File.Move(file, newName);
-                }
+                                System.IO.File.Move(file, newName);
+                            }
+                        }
+                        ClearFiles();
+                    } else
+                    {
+                        return;
+                    }
+                    break;
+                //Replace Text
+                case 1:
+                    if (files != null && files.Length != 0)
+                    {
+                        textToReplace = TextToReplace_Box.Text;
+                        replaceTextWith = ReplaceTextWith_Box.Text;
+                        string newFileName = "";
+
+                        if (textToReplace != null && replaceTextWith != null)
+                        {
+                            foreach (string file in files)
+                            {
+                                if (file.Contains(textToReplace))
+                                {
+                                    newFileName = file.Replace(textToReplace, replaceTextWith);
+                                    System.IO.File.Move(file, newFileName);
+                                }
+                            }
+                        }
+                        ClearFiles();
+                    }
+                    break;
+                default:
+                    break;
             }
         }
+
+        public void ClearFiles()
+        {
+            if (files != null && files.Length != 0)
+            {
+                Array.Clear(files, 0, files.Length);
+            }
+            exampleFile = "";
+            exampleFileExtension = "";
+            exampleString = "";
+
+            fileName = "";
+            fileExtension = "";
+            fileDirectory = "";
+
+            textToAddAfter = "";
+            textToAddBefore = "";
+
+            textToReplace = "";
+            replaceTextWith = "";
+
+    }
 
         private void AfterName_Checked(object sender, RoutedEventArgs e)
         {
@@ -157,6 +246,16 @@ namespace EasyRename
         }
 
         private void BeforeName_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateText();
+        }
+
+        private void TextToReplace_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateText();
+        }
+
+        private void ReplaceTextWith_Box_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateText();
         }
